7fdefbf72a3b945e0ec3973657043d046497ecef
Nicolas <urcocarp1986@gmail.com>
2024-07-08 22:31:26 -0300

    about

---
+++ About/About.jsx
@@ -0,0 +1,13 @@
+import './About.css'
+const About = () => {
+    return (
+      <div className='Content'>
+
+        <div className="about">
+            <h1>hola</h1>
+        </div>
+      </div>
+    );
+}
+
+export default About;
--- App.jsx
+++ App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Hero from './Hero/Hero'
 import Navbar from './Navbar/Navbar'
+import About from './About/About'
 
 function App() {
 return(
   <div>
   <Navbar/>
   <Hero/>
+  <About/>
    
   </div>
 )
