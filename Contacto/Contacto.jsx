86c9cac031d926437baa753a66e79db1bf780d03
Nicolas <urcocarp1986.nr@gmail.com>
2024-08-09 12:27:50 -0300

    listo componententes

--- Contacto/Contacto.jsx
+++ Contacto/Contacto.jsx
@@ -1,10 +1,32 @@
 import "./Contacto.css"
-
+import git from "../assets/github-.svg"
+import linkedin from "../assets/linkedin-.svg"
+import whats from "../assets/whatsapp-.svg"
+import gmail from "../assets/mail-.svg"
 
 const Contacto=()=>{
     return (
         <div className="ContactoConteiner">
-          <h1>Contactar</h1>
+       
+        <div className="Icon">
+          <div >
+            <img src={git}/>
+            
+            
+          </div>
+          <div>
+            <img src={linkedin}/>
+            
+          </div>
+          <div>
+            <img src={gmail}/>
+          
+          </div>
+          <div>
+            <img src={whats}/>
+            
+          </div>
+        </div>
         </div>
     )
 }
